41460de281dd8fe560449008d3d9d60e44b1f761
konczosi <47603441+konczosi@users.noreply.github.com>
2023-07-12 15:42:37 +0200

    Add arg parser function

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", type=open, help="Path to the configuration file.")
+    args = parser.parse_args()
+    return args
+
+
 def main():
-    print("Main")
+    args = parse_args()
+    with args.config as config:
+        # TODO: call Report class from ppt_report package
+        print(type(config))
 
 
 if __name__ == "__main__":
