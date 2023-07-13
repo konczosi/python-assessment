398a52e2bb6e3461e456440fac6feb5beda35dc4
konczosi <47603441+konczosi@users.noreply.github.com>
2023-07-14 00:01:05 +0200

    Add OSError back

--- Task1_PPTX_report/pptx_utils.py
+++ Task1_PPTX_report/pptx_utils.py
@@ -13,5 +13,5 @@ def convert_csv_dat_binary(input_file: str) -> None:
     try:
         df = pd.read_csv(input_file_path, sep=";", header=None)
         np.asarray(df.values).tofile(output_file_path, sep="")
-    except (FileNotFoundError) as error:
+    except (FileNotFoundError, OSError) as error:
         logging.error("Opening %s file failed due to: %s", input_file_path, error)
