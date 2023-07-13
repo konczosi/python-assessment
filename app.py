1c813f89c9ce25817ad30de7347d4a15df1085d7
konczosi <47603441+konczosi@users.noreply.github.com>
2023-07-13 21:59:06 +0200

    Add logging to main

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import argparse
+import logging
 from Task1_PPTX_report import pptx_report
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("config", type=str, help="Path to the configuration file.")
+    parser.add_argument("config", type=str, help="Path/filename to the configuration file.")
     args = parser.parse_args()
     return args
 
 
 def main():
+    logging.basicConfig(filename='myapp.log', level=logging.INFO)
     args = parse_args()
     pptx_report.generate_report(args.config)
 
