e4f6213583bd00522069bbacea2ebac454ae8ab1
konczosi <47603441+konczosi@users.noreply.github.com>
2023-07-13 23:52:54 +0200

    Add exception handling and loggin to csv-binary converter

--- Task1_PPTX_report/pptx_utils.py
+++ Task1_PPTX_report/pptx_utils.py
@@ -1,6 +1,8 @@
+import logging
+import pathlib
+
 import numpy as np
 import pandas as pd
-import pathlib
 
 FILE_PATH_PREFIX = "Task1_PPTX_report/"
 
@@ -8,5 +10,8 @@ def convert_csv_dat_binary(input_file: str) -> None:
     """Open csv file with pandas and save as binary file with numpy."""
     input_file_path = pathlib.Path(FILE_PATH_PREFIX + input_file)
     output_file_path = pathlib.Path(input_file.split(".")[0] + ".dat")
-    df = pd.read_csv(input_file_path, sep=";", header=None)
-    np.asarray(df.values).tofile(output_file_path, sep="")
+    try:
+        df = pd.read_csv(input_file_path, sep=";", header=None)
+        np.asarray(df.values).tofile(output_file_path, sep="")
+    except (FileNotFoundError) as error:
+        logging.error("Opening %s file failed due to: %s", input_file_path, error)
