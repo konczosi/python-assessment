4432456b5e85020143296e14928fb037b99d58f0
konczosi <47603441+konczosi@users.noreply.github.com>
2023-07-13 21:15:38 +0200

    Add generate chart function

--- Task1_PPTX_report/pptx_report.py
+++ Task1_PPTX_report/pptx_report.py
@@ -1,9 +1,15 @@
 import collections
 import collections.abc
 from pptx import Presentation
+from pptx.chart.data import XyChartData
+from pptx.enum.chart import XL_CHART_TYPE
 from pptx.util import Inches
 import json
 import pathlib
+from Task1_PPTX_report import pptx_utils
+import numpy as np
+
+FILE_PATH_PREFIX = "Task1_PPTX_report/"
 
 
 def generate_report(path: str) -> None:
@@ -28,8 +34,8 @@ def generate_slide(sl: dict, prs: Presentation) -> None:  # type: ignore
         generate_list_slide_content(sl, prs)
     elif sl["type"] == "picture":
         generate_picture_slide_content(sl, prs)
-    elif True:
-        pass
+    elif sl["type"] == "plot":
+        generate_chart_slide_content(sl, prs)
     else:
         # TODO handle wrong type -- log the wrong slide type
         pass
@@ -74,7 +80,7 @@ def generate_list_slide_content(sl: dict, prs: Presentation) -> None:  # type: i
 
 
 def generate_picture_slide_content(sl: dict, prs: Presentation) -> None:  # type: ignore
-    img_path = "Task1_PPTX_report/" + sl["content"]
+    img_path = FILE_PATH_PREFIX + sl["content"]
     title_only_slide_layout = prs.slide_layouts[5]
     slide = prs.slides.add_slide(title_only_slide_layout)
     title = slide.shapes.title
@@ -85,11 +91,35 @@ def generate_picture_slide_content(sl: dict, prs: Presentation) -> None:  # type
     slide.shapes.add_picture(img_path, left, top)
 
 
+def generate_chart_slide_content(sl: dict, prs: Presentation) -> None:  # type: ignore
+    input_file_path = sl["content"].split(".")[0] + ".csv"
+    pptx_utils.convert_csv_dat_binary(input_file_path)
+    coords = np.fromfile(sl["content"]).reshape(4, 2)
+    
+    title_only_slide_layout = prs.slide_layouts[5]
+    slide = prs.slides.add_slide(title_only_slide_layout)
+    title = slide.shapes.title
+    title.text = sl["title"]
+    chart_data = XyChartData()
+    series_1 = chart_data.add_series("")
+    for c in coords:
+        series_1.add_data_point(c[0], c[1])
+    x, y, cx, cy = Inches(2), Inches(2), Inches(6), Inches(4.5)
+    
+    chart = slide.shapes.add_chart(
+        XL_CHART_TYPE.XY_SCATTER_LINES_NO_MARKERS, x, y, cx, cy, chart_data  # type: ignore
+    ).chart
+    chart.has_legend = False
+    chart.value_axis.has_major_gridlines = False
+    chart.category_axis.axis_title.text_frame.text = sl["configuration"]["x-label"]
+    chart.value_axis.axis_title.text_frame.text = sl["configuration"]["y-label"]
+
+
 def read_json(path: str) -> dict:
     """Read json file based on the path"""
-    file_path = pathlib.Path(path)
+    config_file_path = pathlib.Path(path)
 
-    with file_path.open("r") as file:
+    with config_file_path.open("r") as file:
         config = json.load(file)
 
     return config
--- Task1_PPTX_report/pptx_utils.py
+++ Task1_PPTX_report/pptx_utils.py
@@ -3,17 +3,10 @@ import pandas as pd
 import pathlib
 
 FILE_PATH_PREFIX = "Task1_PPTX_report/"
-DATA_DIR = "data/"
-
 
 def convert_csv_dat_binary(input_file: str) -> None:
     """Open csv file with pandas and save as binary file with numpy."""
-    # Create data directory if not extist.
-    pathlib.Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
-
     input_file_path = pathlib.Path(FILE_PATH_PREFIX + input_file)
-    output_file_path = pathlib.Path(
-        DATA_DIR + input_file.split(".")[0] + ".dat"
-    )
+    output_file_path = pathlib.Path(input_file.split(".")[0] + ".dat")
     df = pd.read_csv(input_file_path, sep=";", header=None)
     np.asarray(df.values).tofile(output_file_path, sep="")
